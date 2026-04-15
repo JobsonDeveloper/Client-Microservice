9cca2422cc081e7e4e6bc188f7a0f5cf2a97c627
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-04-14 22:02:55 -0300

    Feature: Add a template to email message about the sale cancelled

--- src/main/java/br/com/client/micro/service/IHtmlMessageTemplateService.java
+++ src/main/java/br/com/client/micro/service/IHtmlMessageTemplateService.java
@@ -5,4 +5,5 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 public interface IHtmlMessageTemplateService {
     public String paymentProcessedTemplate(String clientName);
     public String deliveryCompletedTemplate(String clientName);
+    public String saleCanceledTemplate(String clientName);
 }
